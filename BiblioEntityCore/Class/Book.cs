4888fd7315d921e1f887cacdced72002a659636d
Devconte <romain.lcnte@gmail.com>
2025-02-10 17:09:15 +0100

    feat: added author and book classes

--- BiblioEntityCore/Class/Book.cs
+++ BiblioEntityCore/Class/Book.cs
@@ -2,5 +2,9 @@ namespace BiblioEntityCore.Class;
 
 public class Book
 {
-    
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public int AuthorId { get; set; }
+    // navigation property, allow to create the relationship with the books. https://learn.microsoft.com/en-us/ef/core/modeling/relationships
+    public Author Author { get; set; } 
 }
