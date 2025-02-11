6990e7692f4ab68e57ed13c5946790f70fcb26a4
Devconte <romain.lcnte@gmail.com>
2025-02-11 09:56:15 +0100

    updated relationship within book and auhor

--- BiblioEntityCore/Class/Book.cs
+++ BiblioEntityCore/Class/Book.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BiblioEntityCore.Class;
 
 public class Book
+
 {
+    [Key]
     public int Id { get; set; }
     public string Title { get; set; } = "";
     public int AuthorId { get; set; }
     // navigation property, allow to create the relationship with the books. https://learn.microsoft.com/en-us/ef/core/modeling/relationships
+    [ForeignKey("AuthorId")]
     public Author Author { get; set; } 
 }
