75ccfd076d2083644ae7930f0ef05607053e7fef
Devconte <romain.lcnte@gmail.com>
2025-02-11 11:10:49 +0100

    reorganized code and added add author

--- BiblioEntityCore/Program.cs
+++ BiblioEntityCore/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using BiblioEntityCore.Class;
+using BiblioEntityCore.Migrations;
 
 namespace BiblioEntityCore;
 
@@ -10,10 +11,45 @@ public class Program
     {
         using (AppDbContext context = new AppDbContext())
         {
-        
-            Console.WriteLine("Hello World!");
+
+
+            while (true)
+            {
+                Console.WriteLine("\n1. Add Author\n2. Add Book\n3. List Authors\n4. List Books\n5. Delete Author\n6. Delete Book\n7. Statistics\n8. Exit"
+            );
+            var choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    AddAuthor(context);
+                    break;
+                case "8":
+                    return;
+                default:
+                    Console.WriteLine("Choix invalide !");
+                    break;
+            }
             
+            
+            }
+        } 
+        
+    }
+
+    private static void AddAuthor(AppDbContext context)
+    {
+        Console.WriteLine("Enter Author Name: ");
+        string name = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            Console.WriteLine("Name cannot be empty");
+            return;
         }
 
+        var author = new Author { Name = name };
+        context.Authors.Add(author);
+        context.SaveChanges();
+        Console.WriteLine($"Author '{name}' added");
     }
 }
